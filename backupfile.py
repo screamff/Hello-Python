9f25d6d8cc5be85d716e003cb61dd9deb49e21aa
screamff <30379611+screamff@users.noreply.github.com>
2019-04-01 15:33:37 +0800

    add
    快速存档备份，载入指定存档

--- backupfile.py
+++ backupfile.py
@@ -7,7 +7,8 @@ import os
 # 2.创建备份文件夹，copy原文件
 # 3.rollback功能
 
-def backup(file):
+
+def quickbackup(file):
     """file为存档文件路径"""
     try:
         with open(file, 'rb') as f:
@@ -18,16 +19,20 @@ def backup(file):
         print(e)
     else:
         filename = os.path.basename(file)
-        p_filepath = os.path.dirname(file)
-        backup_file = p_filepath + '\\backup\\' + filename
-        if os.path.exists(p_filepath+'\\backup'):
+        foldername = os.path.dirname(file)
+        backup_file = foldername + '\\backup\\' + filename
+        if os.path.exists(foldername+'\\backup'):
             pass
         else:
-            os.mkdir(p_filepath+'\\backup')
+            os.mkdir(foldername+'\\backup')
         with open(backup_file, 'wb+') as a:
             a.write(save)
+        print('-----*-----')
+        print("快速备份完成！")
+        print('-----*-----')
 
-def rollback(file):
+
+def quickrollback(file):
     filename = os.path.basename(file)
     p_filepath = os.path.dirname(file)
     backup_file = p_filepath + '\\backup\\' + filename
@@ -41,15 +46,65 @@ def rollback(file):
     else:
         with open(file, 'wb+') as a:
             a.write(save)
+        print('-----*-----')
+        print("快速载入完成!")
+        print('-----*-----')
+
+
+def rollback(save, rawfile):
+    """save为要载入的存档路径，rawfile为原存档路径"""
+    try:
+        with open(save, 'rb') as f:
+            save = f.read()
+    except FileNotFoundError:
+        print("文件不存在")
+    except Exception as e:
+        print(e)
+    else:
+        with open(rawfile, 'wb+') as a:
+            a.write(save)
+        print('-----*-----')
+        print("载入成功!")
+        print('-----*-----')
+
+
+def loadsave(file):
+    """载入所有存档文件夹,file为原存档文件"""
+    savefilename = os.path.basename(file)
+    foldername = os.path.dirname(file)
+    folders = os.listdir(foldername)
+    folders = list(map(lambda x: os.path.join(foldername, x), folders))
+    # print('folders:', folders)
+    savefolders = []
+    for i in folders:
+        if os.path.isdir(i):
+            # print("isdir:", i)
+            savefolders.append(os.path.join(i, savefilename))
+    savefolders = sorted(savefolders,  key=lambda x: os.path.getctime(x))
+    for i in range(len(savefolders)):
+        print("%d--%s" % (i, os.path.basename(
+                                             os.path.dirname(savefolders[i]))))
+    choice = input("输入要载入的存档编号:")
+    rollback(savefolders[int(choice)], file)
+
+
+def main():
+    file = input("输入文件路径:")
+    while True:
+        print('-----选择功能-----')
+        print('1--快速备份')
+        print('2--快速读取')
+        print('3--新建备份')
+        print('4--载入存档')
+        print('------------------')
+        choice = input('输入功能编号:')
+        if choice == '1':
+            quickbackup(file)
+        if choice == '2':
+            quickrollback(file)
+        if choice == '4':
+            loadsave(file)
+
 
 if __name__ == '__main__':
-    file = input("输入文件路径:")
-    print('-----选择功能-----')
-    print('1--备份文件')
-    print('2--还原文件')
-    print('------------------')
-    choice = input('输入功能编号:')
-    if choice == '1':
-        backup(file)
-    if choice == '2':
-        rollback(file)
+    main()
